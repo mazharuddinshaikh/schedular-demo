71d6d8366b5eda6ba43e1f75eae5886bc29bd222
skmazharuddin <skmazharuddin1111@gmail.com>
2020-10-03 17:54:24 +0530

    error commit test

--- src/main/java/com/mazze/schedular/SchedularDemoApplication.java
+++ src/main/java/com/mazze/schedular/SchedularDemoApplication.java
@@ -9,6 +9,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class SchedularDemoApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Hello World")
 		new SpringApplicationBuilder(SchedularDemoApplication.class).headless(false).run(args);
 	}
 
