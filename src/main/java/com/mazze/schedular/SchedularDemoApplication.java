cb5b22175493b2a743bd5d3871fed2bec52d0e8d
skmazharuddin <skmazharuddin1111@gmail.com>
2020-10-07 12:42:02 +0530

    test commit for jenkin trigger build

--- src/main/java/com/mazze/schedular/SchedularDemoApplication.java
+++ src/main/java/com/mazze/schedular/SchedularDemoApplication.java
@@ -1,15 +1,21 @@
 package com.mazze.schedular;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
 @SpringBootApplication
-public class SchedularDemoApplication {
+public class SchedularDemoApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		new SpringApplicationBuilder(SchedularDemoApplication.class).headless(false).run(args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Jenkin test commit");
 	}
 
 }
